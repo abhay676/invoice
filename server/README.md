16745b2b8c342a9a2715d580c8b3f11968f499a1
Abhay Goswami <abhay@roadzen.io>
2022-05-23 19:53:19 +0530

    add exclude in company & update Readme

--- server/README.md
+++ server/README.md
@@ -2,6 +2,7 @@
 
 ---
 
+1. Setup Role based Authorization
 2. Able to create `Admin` user
 3. Able to invite user via SMS & email
 4. Able to create dynamic invoices
@@ -9,6 +10,5 @@
 6. Able to delete uploaded invoices via cron-job
 7. Able to update user information
 8. Able to update company information `ONLY` via `Admin`
-9. Role based access
-10. Dashboard API's
-11. Update Readme
+9. Dashboard API's
+10. Update Readme
--- server/src/company/entities/company.entity.ts
+++ server/src/company/entities/company.entity.ts
@@ -7,8 +7,10 @@ import {
   OneToMany,
 } from 'typeorm';
 import { User } from 'src/users/entities/user.entity';
+import { Exclude, Expose } from 'class-transformer';
 @Entity({ name: 'company' })
 export class Company {
+  @Exclude()
   @PrimaryGeneratedColumn('increment')
   id: number;
 
@@ -18,18 +20,23 @@ export class Company {
   @Column({ nullable: false, enum: ['₹', '$', '€'] })
   currency: string;
 
+    @Exclude()
   @Column({ nullable: false })
   address: string;
 
+  @Exclude()
   @Column({ nullable: false })
   state: string;
 
+  @Exclude()
   @Column({ nullable: false })
   country: string;
 
+  @Exclude()
   @Column({ nullable: false })
   pincode: number;
 
+  @Exclude()
   @OneToMany(() => User, (user) => user.id)
   users: User[];
 
@@ -38,4 +45,13 @@ export class Company {
 
   @UpdateDateColumn()
   updatedAt: Date;
+
+  @Expose()
+  get addr(): string {
+    return `${this.address}, ${this.state}, ${this.pincode}`;
+  }
+
+  constructor(partial: Partial<User>) {
+    Object.assign(this, partial);
+  }
 }
